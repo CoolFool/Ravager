d88536264c2bfef1d8ff1c7927031682e2a00aed
coolfool <coolfool149@gmail.com>
2022-02-05 02:50:22 +0530

    changed start order for celery and bot

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ python config.py
 python housekeeping.py
 python services/aria/handler.py &
 P1=$!
-gunicorn -b 0.0.0.0:${PORT} main:app &
-P2=$!
 celery -A ravager.celery_tasks.tasks purge -f
 celery -A ravager.celery_tasks.tasks  worker -B  --loglevel info --concurrency 5 --autoscale=10,5 --logfile ../logs/celery.log --events --pool prefork &
+P2=$!
+gunicorn -b 0.0.0.0:${PORT} main:app &
 P3=$!
 wait $P1 $P2 $P3
